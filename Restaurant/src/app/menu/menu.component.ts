dd2f06bbe303d855659a416df931258a51e2098d
Michael-Mueller-Vienna <michael.mueller.bf@gmail.com>
2023-03-03 17:39:56 +0100

    step 4 1/2 menu (uncomplete)

--- Restaurant/src/app/menu/menu.component.ts
+++ Restaurant/src/app/menu/menu.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { products } from "../products"
+import { Iproducts } from "../Iproducts"
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent {
 
+export class MenuComponent implements OnInit {
+
+  products:Iproducts[] = products;
+
+  product:Iproducts = {} as Iproducts;
+  id:number = 0;
 }
