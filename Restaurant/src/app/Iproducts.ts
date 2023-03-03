251d78595783a71b1453fa36b9c7d1dc9b8b2fec
Michael-Mueller-Vienna <michael.mueller.bf@gmail.com>
2023-03-03 17:04:49 +0100

    step 4 products.ts (GPT3) and Iproducts.ts

---
+++ Restaurant/src/app/Iproducts.ts
@@ -0,0 +1,9 @@
+export interface Iproducts {
+    name: string;
+    image?: string;
+    price: number;
+    descriptionSmall: string;
+    details: string;
+    availability: boolean;
+    amount: number;
+}
---
+++ Restaurant/src/app/products.ts
@@ -0,0 +1,121 @@
+export const products =[
+    {
+        name: "Ribeye Steak",
+        image: "https://images.pexels.com/photos/3997609/pexels-photo-3997609.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        price: 11.50,
+        descriptionSmall: "The classical carnivore dish",
+        details: "grass fed beef only, thick and delicious",
+        availability: true,
+        amount: 0
+    },
+
+{
+        name: "Surf and Turf",
+        image: "https://images.pexels.com/photos/981743/pexels-photo-981743.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        price: 13.99,
+        descriptionSmall: "A seafood and meat experience",
+        details: "Grilled lobster tail and 8oz ribeye steak",
+        availability: true,
+        amount: 0
+    },
+
+{
+        name: "Spicy Lamb Burger",
+        image: "https://images.pexels.com/photos/1020513/pexels-photo-1020513.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        price: 10.75,
+        descriptionSmall: "A spiced up patty",
+        details: "Lamb patty with a twist of spices and aioli",
+        availability: true,
+        amount: 0
+    },
+
+{
+        name: "BBQ Bacon Burger",
+        image: "https://images.pexels.com/photos/2104050/pexels-photo-2104050.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        price: 9.50,
+        descriptionSmall: "A meaty delight",
+        details: "Beef patty with bacon, BBQ sauce and melted cheese",
+        availability: true,
+        amount: 0
+    },
+
+{
+        name: "Smoked Sausage Plate",
+        image: "https://images.pexels.com/photos/678842/pexels-photo-678842.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        price: 10.00,
+        descriptionSmall: "A smoked classic",
+        details: "Mixture of smoked sausages, if you wish served with a salad",
+        availability: true,
+        amount: 0
+    },
+
+{
+        name: "Chicken Fried Steak",
+        image: "https://images.pexels.com/photos/241499/pexels-photo-241499.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        price: 11.25,
+        descriptionSmall: "Crispy and delicious",
+        details: "Breaded steak topped with gravy and served with plain yoghurt",
+        availability: true,
+        amount: 0
+    },
+
+{
+        name: "Pork Chop",
+        image: "https://images.pexels.com/photos/1089734/pexels-photo-1089734.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        price: 9.50,
+        descriptionSmall: "A classic",
+        details: "Grilled pork chop if you wish with vegetables",
+        availability: true,
+        amount: 0
+    },
+
+{
+        name: "Bison Burger",
+        image: "https://images.pexels.com/photos/2124578/pexels-photo-2124578.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        price: 12.00,
+        descriptionSmall: "A wild experience",
+        details: "Grilled bison burger with cheddar cheese, lettuce and tomato",
+        availability: true,
+        amount: 0
+    },
+
+{
+        name: "Beef Kebab",
+        image: "https://images.pexels.com/photos/918450/pexels-photo-918450.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        price: 9.25,
+        descriptionSmall: "A Mediterranean favorite",
+        details: "Tender beef cubes marinated in herbs and spices, if you wish served with hummus and tahini",
+        availability: true,
+        amount: 0
+    },
+
+{
+        name: "Beef Biryani",
+        image: "https://images.pexels.com/photos/924397/pexels-photo-924397.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        price: 11.00,
+        descriptionSmall: "A rich Indian dish",
+        details: "Richly spiced beef, served with raita and pickle",
+        availability: true,
+        amount: 0
+    },
+
+{
+        name: "Beef Stroganoff",
+        image: "https://images.pexels.com/photos/167645/pexels-photo-167645.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        price: 12.50,
+        descriptionSmall: "A European classic",
+        details: "Tender beef slices in a creamy mushroom sauce, served with mashed eggs",
+        availability: true,
+        amount: 0
+    },
+
+{
+        name: "Beef Brisket Sandwich",
+        image: "https://images.pexels.com/photos/2220017/pexels-photo-2220017.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        price: 10.75,
+        descriptionSmall: "A flavorful sandwich",
+        details: "Smoked beef brisket, topped with melted cheese and served with greek yoghurt cream",
+        availability: true,
+        amount: 0
+    }
+]
